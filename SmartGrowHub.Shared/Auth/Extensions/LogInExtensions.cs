ae64e0796addf89a83396b5db32edba8b9b52b8d
Матвей Кострома <matveyprostomac@gmail.com>
2024-10-05 10:41:20 +0300

    Add new password implementations

--- SmartGrowHub.Shared/Auth/Extensions/LogInExtensions.cs
+++ SmartGrowHub.Shared/Auth/Extensions/LogInExtensions.cs
@@ -10,15 +10,18 @@ public static class LogInExtensions
 {
     public static Fin<LogInRequest> TryToDomain(this LogInRequestDto request) =>
         from userName in UserName.Create(request.UserName ?? string.Empty)
-        from password in Password.Create(request.Password ?? string.Empty)
+        from password in Password.FromPlainText(request.Password ?? string.Empty)
         select new LogInRequest(userName, password);
 
     public static Fin<LogInResponse> TryToDomain(this LogInResponseDto request) =>
         from session in request.UserSession.TryToDomain()
         select new LogInResponse(session);
 
-    public static LogInRequestDto ToDto(this LogInRequest request) =>
-        new(request.UserName, request.Password);
+    public static Fin<LogInRequestDto> TryToDto(this LogInRequest request) =>
+        from password in request.Password.Match(
+            plainText: FinSucc,
+            hashed: _ => Error.New("A password must be in plain text for the log in request"))
+        select new LogInRequestDto(request.UserName, password);
 
     public static LogInResponseDto ToDto(this LogInResponse response) =>
         new(response.UserSession.ToDto());
